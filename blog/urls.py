d9d27f42d5bda83314f39b99845d2d8192e5b56b
Janaina <janna_silveira@yahoo.com.br>
2025-04-21 13:08:50 -0300

    incluindo templates portao

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-#  path('', views.portao),
+    path('portao', views.portao),
     path('', views.post_list, name='post_list'),
     path('sala', views.sala),
     path('quarto', views.quarto)
--- blog/views.py
+++ blog/views.py
@@ -4,14 +4,17 @@ from django.http import HttpResponse
 # View é tudo aquilo que recebe uma request e retorna uma response
 # Create your views here.
 
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
+
 def portao(request):
-    return HttpResponse("Você chegou ao portão da casa")
+    return render(request, 'blog/porta.html', {})
+
+#def portao(request):
+  #  return HttpResponse("Você chegou ao portão da casa")
 
 def sala(request):
     return HttpResponse("Você chegou na sala")
 
 def quarto(request):
     return HttpResponse("Você chegou no quarto")
-
-def post_list(request):
-    return render(request, 'blog/post_list.html', {})
