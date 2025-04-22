161fe4bf7eb5928d4636c68d19cf8f9eb87c1690
Janaina <janna_silveira@yahoo.com.br>
2025-04-22 20:39:50 -0300

    css, html...

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
+from django.utils import timezone
 
 # View é tudo aquilo que recebe uma request e retorna uma response
 # Create your views here.
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    # da tabela posts, filtra os objetos que tem a data de publicação maior do que o momeno de agora (timezone.now()), ordenado pela data de publicação.
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+
+    return render(request, 'blog/post_list.html', {"posts": posts})
 
 def portao(request):
     return render(request, 'blog/porta.html', {})
--- mysite/settings.py
+++ mysite/settings.py
@@ -121,6 +121,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
